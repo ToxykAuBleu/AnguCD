403c0a0666acecda241b88e7a83b405380598796
ToxykAuBleu <foissac.mathieu@gmail.com>
2024-03-25 14:20:58 +0100

    [~] Modification du composant list-cd afin d'implémenter le service.

--- src/app/list-cd/list-cd.component.ts
+++ src/app/list-cd/list-cd.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CD } from '../models/cd.model';
+import { CdsService } from '../services/cds.service';
 
 @Component({
   selector: 'app-list-cd',
@@ -9,36 +10,9 @@ import { CD } from '../models/cd.model';
 export class ListCDComponent implements OnInit {
   listcd! : CD[];
 
+  constructor (private cdsService: CdsService) { }
+
   ngOnInit(): void {
-    this.listcd = [
-      {
-        id: 1,
-        title: 'The Dark Side of the Moon',
-        author: 'Pink Floyd',
-        price: 10,
-        thumbnail: 'https://upload.wikimedia.org/wikipedia/en/3/3b/Dark_Side_of_the_Moon.png',
-        dateDeSortie: new Date(1973, 3, 1),
-        quantite: 1,
-      }, 
-      {
-        id: 2,
-        title: 'Pulse',
-        author: 'Pink Floyd',
-        price: 10,
-        thumbnail: 'https://pinkfloydhyperbase.dk/illu/covers/pulse.jpg',
-        dateDeSortie: new Date(1974, 4, 11),
-        quantite: 2,
-        onsale: true
-      },
-      {
-        id: 3,
-        title: 'Pulse1',
-        author: 'Pink Floyd',
-        price: 100,
-        thumbnail: 'https://pinkfloydhyperbase.dk/illu/covers/pulse.jpg',
-        dateDeSortie: new Date(1984, 6, 20),
-        quantite: 0
-      },
-    ];
+    this.listcd = this.cdsService.getCDs();
   }
 }
