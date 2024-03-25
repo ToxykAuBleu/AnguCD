10e83c90df36ba6b2803771bdb57ad2aa3d4e9e7
ToxykAuBleu <foissac.mathieu@gmail.com>
2024-03-25 14:15:56 +0100

    [+] Ajout d'une liste de CD dans le service CD.

--- src/app/services/cds.service.ts
+++ src/app/services/cds.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { CD } from '../models/cd.model';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +7,37 @@ import { Injectable } from '@angular/core';
 export class CdsService {
 
   constructor() { }
+
+  getCDs(): CD[] {
+    return [
+      {
+        id: 1,
+        title: 'The Dark Side of the Moon',
+        author: 'Pink Floyd',
+        price: 10,
+        thumbnail: 'https://upload.wikimedia.org/wikipedia/en/3/3b/Dark_Side_of_the_Moon.png',
+        dateDeSortie: new Date(1973, 3, 1),
+        quantite: 1,
+      }, 
+      {
+        id: 2,
+        title: 'Pulse',
+        author: 'Pink Floyd',
+        price: 10,
+        thumbnail: 'https://pinkfloydhyperbase.dk/illu/covers/pulse.jpg',
+        dateDeSortie: new Date(1974, 4, 11),
+        quantite: 2,
+        onsale: true
+      },
+      {
+        id: 3,
+        title: 'Pulse1',
+        author: 'Pink Floyd',
+        price: 100,
+        thumbnail: 'https://pinkfloydhyperbase.dk/illu/covers/pulse.jpg',
+        dateDeSortie: new Date(1984, 6, 20),
+        quantite: 0
+      },
+    ];
+  }
 }
