672af8901e7bffb17c6fd30b3ca3c2c4ac848c5d
ToxykAuBleu <foissac.mathieu@gmail.com>
2024-03-27 10:49:27 +0100

    [~] Optimisation de la navbar.

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,5 +1,5 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -8,27 +8,13 @@ import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 })
 export class HeaderComponent {
   title = "AnguCD";
-  
-  @ViewChild("routeAccueil") routeAccueil!: ElementRef;
-  @ViewChild("routeCatalogue") routeCatalogue!: ElementRef;
-  @ViewChild("routeAjouter") routeAjouter!: ElementRef;
+  public currentRoute!: string;  
 
   constructor(private router: Router) {
     this.router.events.subscribe((event) => {
       if (event instanceof NavigationEnd) {
-        this.switchSelectedTab(event.url);
+        this.currentRoute = event.url;
       }
     });
-  }
-
-  switchSelectedTab(routeUrl: string) {
-    const routes = [this.routeAccueil, this.routeCatalogue, this.routeAjouter];
-    for (const route of routes) {
-      if (route.nativeElement.getAttribute("routerlink") === routeUrl) {
-        route.nativeElement.classList.add("link-secondary");
-      } else {
-        route.nativeElement.classList.remove("link-secondary");
-      }
-    }
   }
 }
