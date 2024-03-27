85bdd4d5421f46e0b7a51561474eddf3b9e8f120
ToxykAuBleu <foissac.mathieu@gmail.com>
2024-03-27 10:14:25 +0100

    [+] Ajout du style sur la navbar.

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -7,4 +8,27 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
   title = "AnguCD";
+  
+  @ViewChild("routeAccueil") routeAccueil!: ElementRef;
+  @ViewChild("routeCatalogue") routeCatalogue!: ElementRef;
+  @ViewChild("routeAjouter") routeAjouter!: ElementRef;
+
+  constructor(private router: Router) {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.switchSelectedTab(event.url);
+      }
+    });
+  }
+
+  switchSelectedTab(routeUrl: string) {
+    const routes = [this.routeAccueil, this.routeCatalogue, this.routeAjouter];
+    for (const route of routes) {
+      if (route.nativeElement.getAttribute("routerlink") === routeUrl) {
+        route.nativeElement.classList.add("link-secondary");
+      } else {
+        route.nativeElement.classList.remove("link-secondary");
+      }
+    }
+  }
 }
